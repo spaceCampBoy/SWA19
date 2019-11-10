2292cf4ffc88f546e85c276cfa1a046ea6127d0f
spaceCampBoy <260101@via.dk>
2019-11-10 14:29:00 +0100

    react completed

--- assignment3/react-flux-weather-app/src/index.js
+++ assignment3/react-flux-weather-app/src/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {WeatherData} from './model'
+import store from './WeatherStore'
+import view from './WeatherView'
+import dispatcher from './WeatherDispatcher'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+async function fetchWeather(historicOrForecast) {
+    return await fetch(`http://localhost:8080/${historicOrForecast}`)
+        .then(res => res.ok ? res.json() : Promise.reject(res.statusText))
+}
 
+async function init() {
+    try {
+        const historicData = await fetchWeather("Data")
+        const forecast = await fetchWeather("Forecast")
+        const theModel = WeatherData(historicData, forecast)
+        let renderer = dom => ReactDOM.render(dom, document.getElementById('root'))
+        let theDispatcher
+        const theView = view(() => theDispatcher)
+        const theStore = store(theModel, theView, renderer)
+        theDispatcher = dispatcher(theStore)
+        renderer(theView(theModel))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+init()
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
